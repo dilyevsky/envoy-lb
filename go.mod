a908a426771324f5184cb6e59a1427b654813def
Dmitry Ilyevskiy <dilyevsky@mux.com>
2020-06-17 14:33:01 -0700

    init commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/mux/envoy-lb
+
+go 1.14
+
+require (
+	github.com/envoyproxy/go-control-plane v0.9.5
+	github.com/sirupsen/logrus v1.6.0
+	google.golang.org/grpc v1.29.1
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"net"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+
+	api "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
+	"github.com/envoyproxy/go-control-plane/pkg/cache/v2"
+	xds "github.com/envoyproxy/go-control-plane/pkg/server/v2"
+)
+
+func (cb *callbacks) Report() {
+	cb.mu.Lock()
+	defer cb.mu.Unlock()
+	log.WithFields(log.Fields{"fetches": cb.fetches, "requests": cb.requests}).Info("cb.Report()  callbacks")
+}
+func (cb *callbacks) OnStreamOpen(ctx context.Context, id int64, typ string) error {
+	log.Infof("OnStreamOpen %d open for %s", id, typ)
+	return nil
+}
+func (cb *callbacks) OnStreamClosed(id int64) {
+	log.Infof("OnStreamClosed %d closed", id)
+}
+func (cb *callbacks) OnStreamRequest(int64, *api.DiscoveryRequest) error {
+	log.Infof("OnStreamRequest")
+	cb.mu.Lock()
+	defer cb.mu.Unlock()
+	cb.requests++
+	if cb.signal != nil {
+		close(cb.signal)
+		cb.signal = nil
+	}
+	return nil
+}
+func (cb *callbacks) OnStreamResponse(int64, *api.DiscoveryRequest, *api.DiscoveryResponse) {
+	log.Infof("OnStreamResponse...")
+	cb.Report()
+}
+func (cb *callbacks) OnFetchRequest(ctx context.Context, req *api.DiscoveryRequest) error {
+	log.Infof("OnFetchRequest...")
+	cb.mu.Lock()
+	defer cb.mu.Unlock()
+	cb.fetches++
+	if cb.signal != nil {
+		close(cb.signal)
+		cb.signal = nil
+	}
+	return nil
+}
+func (cb *callbacks) OnFetchResponse(*api.DiscoveryRequest, *api.DiscoveryResponse) {}
+
+type callbacks struct {
+	signal   chan struct{}
+	fetches  int
+	requests int
+	mu       sync.Mutex
+}
+
+func main() {
+	snapshotCache := cache.NewSnapshotCache(false, cache.IDHash{}, nil)
+	server := xds.NewServer(context.Background(), snapshotCache, nil)
+	grpcServer := grpc.NewServer()
+	lis, _ := net.Listen("tcp", ":8080")
+
+	discovery.RegisterAggregatedDiscoveryServiceServer(grpcServer, server)
+	api.RegisterEndpointDiscoveryServiceServer(grpcServer, server)
+	api.RegisterClusterDiscoveryServiceServer(grpcServer, server)
+	api.RegisterRouteDiscoveryServiceServer(grpcServer, server)
+	api.RegisterListenerDiscoveryServiceServer(grpcServer, server)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
